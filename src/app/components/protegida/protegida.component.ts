efd535744475edad489499639f2fa86289c77257
otnielmunozdev <otniel.diazescobarmunoz@gmail.com>
2019-09-12 13:22:48 -0500

    Subiendo Auth con Angular

---
+++ src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-protegida',
+  templateUrl: './protegida.component.html',
+  styles: []
+})
+export class ProtegidaComponent implements OnInit {
+
+  constructor(public auth:AuthService) { //para poder usar las propiedades en el html
+
+   }
+
+  ngOnInit() {
+    console.log("NgOninit protegida");
+    
+    this.auth.userProfile$.subscribe(perfil=>{
+      console.log(perfil);
+      
+    })
+  }
+
+}
